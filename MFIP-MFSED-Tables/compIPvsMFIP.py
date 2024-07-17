ab7115a6b721084b205641db7c5fdbf0ca497f89
emretosn <emre7osun@gmail.com>
2024-07-17 16:41:42 +0200

    bip testing

--- MFIP-MFSED-Tables/compIPvsMFIP.py
+++ MFIP-MFSED-Tables/compIPvsMFIP.py
@@ -6,39 +6,28 @@ import pickle
 import gc
 import os
 
-
-
-
 from utils import *
 from mfbrTabs import *
 
 
-
-
 def main():
-
-
     now = datetime.now()
     timeTag = now.strftime("%d%m%Y_%H%M%S")
 
     bordersDir = f'./lookupTables/Borders/'
-    tabMFIPdir = f'./lookupTables/MFIP/'
-
-
+    tabQMFIPdir = f'./lookupTables/MFIPSubRand/'
+    tabRanddir = f'./lookupTables/Rand/'
 
     numSamples = 2*100000
     idSynSamples = np.arange(0,numSamples,2, dtype=int)
 
     print('Normalized samples\nnumSamples = ', numSamples)
 
-    # can get rid of some of them try to play with it
-    dimFlist = [32, 64, 128, 256, 512]
-    nBList = np.arange(2,13)
-    dQList = [0.01, 0.001] # [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001]
-
+    dimFlist = [32, 64, 128]
+    nBList = np.arange(2,4)
+    dQList = [0.001]
 
     for dimF in dimFlist:
-
         pearson = {d:[] for d in dQList}
 
         resDir = f'./results/Synthetic/IP/dimF_{dimF}'
@@ -50,10 +39,11 @@ def main():
 
         for nB in nBList:
             borders = pickle.load(open(f'{bordersDir}/Borders_nB_{nB}_dimF_{dimF}.pkl', 'rb'))
-            tabMFIP = pickle.load(open(f'{tabMFIPdir}/MFIP_nB_{nB}_dimF_{dimF}.pkl', 'rb'))
+            tabQMFIP = pickle.load(open(f'{tabQMFIPdir}/MFIPSubRand_nB_{nB}_dimF_{dimF}.pkl', 'rb'))
+            tabRand = pickle.load(open(f'{tabRanddir}/Rand_nB_{nB}_dimF_{dimF}.pkl', 'rb'))
+
             for dQ in dQList:
-                tabQMFIP = np.round(tabMFIP/dQ).astype(int)
-                fct_args = zip(idSynSamples, repeat(synSamples), repeat(borders), repeat(tabQMFIP))
+                fct_args = zip(idSynSamples, repeat(synSamples), repeat(borders), repeat(tabQMFIP), repeat(tabRand))
 
                 pool = mp.Pool(32)
                 scoresIPandIPQ = pool.starmap(compIPandIPQ, fct_args)
@@ -77,14 +67,6 @@ def main():
 
         plotPearson(f'{resDir}/Pearson_IP_{dimF}.pdf', f'MFIP (dimF = {dimF})', nBList, pearson)
         pickle.dump((nBList, dQList, pearson), open(f'{resDir}/results_dimF_{dimF}_{timeTag}.pkl', 'wb'))
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
