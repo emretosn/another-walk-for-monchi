312e465597495c5450caa2514d159eefc9952da9
emretosn <emre7osun@gmail.com>
2024-08-27 15:41:11 +0200

    test real reference

--- test/test.go
+++ test/test.go
@@ -1,49 +1,43 @@
 package main
 
 import (
+    "os"
 	"fmt"
     "math"
-	"math/bits"
     "math/rand"
-    "time"
+    "encoding/csv"
+    "strconv"
+	//"math/bits"
+    //"math/rand"
 
 	"github.com/tuneinsight/lattigo/v4/bfv"
 	"github.com/tuneinsight/lattigo/v4/rlwe"
 )
 
+func (Enrollment *Enrollment_s) optimizedPlaintextMul(arr []int64) *bfv.PlaintextMul {
+    plainMask := bfv.NewPlaintextMul(Enrollment.params, Enrollment.params.MaxLevel())
+    Enrollment.encoder.EncodeMul(arr, plainMask)
+    return plainMask
+}
+
 func (BIP *BIP_s) getFinalScoreCT(Enrollment *Enrollment_s, permRefTempCT *rlwe.Ciphertext, permProbeTempMask []int64) *rlwe.Ciphertext {
     ringDim := BIP.params.N()
     halfRing := float64(ringDim / 2)
-    //fmt.Println(ringDim)
+    fmt.Println("halfRing", halfRing)
 
-    permProbeTempMaskPT := bfv.NewPlaintext(BIP.params, BIP.params.MaxLevel())
-    Enrollment.encoder.Encode(permProbeTempMask, permProbeTempMaskPT)
+    permProbeTempMaskPT := Enrollment.optimizedPlaintextMul(permProbeTempMask)
+    finalScoreCT := BIP.evaluator.MulNew(permRefTempCT, permProbeTempMaskPT)
+
     maskedRefTempCT := BIP.evaluator.MulNew(permRefTempCT, permProbeTempMaskPT)
 
-    finalScoreCT := maskedRefTempCT
+    finalScoreCT = maskedRefTempCT
 
-    //fmt.Println("finalScoreCT")
-    //finalScorePT := BIP.decryptor.DecryptNew(finalScoreCT)
-    //finalScoreVec := Enrollment.encoder.DecodeUintNew(finalScorePT)
-    //fmt.Println(len(finalScoreVec))
-    //fmt.Println(finalScoreVec[:16])
-
+    fmt.Println(int(math.Log2(halfRing)))
     for i := 0; i < int(math.Log2(halfRing)); i++ {
 		rotation := int(math.Pow(2, float64(i)))
-        //fmt.Println(rotation)
 
 		rotatedCT := BIP.evaluator.RotateColumnsNew(finalScoreCT, rotation)
 		BIP.evaluator.Add(finalScoreCT, rotatedCT, finalScoreCT)
-
-        //fmt.Println("finalScorerotCT")
-        //finalScorerotPT := BIP.decryptor.DecryptNew(rotatedCT)
-        //finalScorerotVec := Enrollment.encoder.DecodeUintNew(finalScorerotPT)
-        //fmt.Println(finalScorerotVec[:16])
-
-        //fmt.Println("finalScoreCT")
-        //finalScorePT = BIP.decryptor.DecryptNew(finalScoreCT)
-        //finalScoreVec = Enrollment.encoder.DecodeUintNew(finalScorePT)
-        //fmt.Println(finalScoreVec[:16])
 	}
 
 	return finalScoreCT
@@ -69,55 +63,190 @@ type Enrollment_s struct {
 	encryptor rlwe.Encryptor
 }
 
+func readCSVTo2DSlice(filename string) ([][]int64, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open file: %v", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	var result [][]int64
+
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			return nil, fmt.Errorf("unable to read CSV file: %v", err)
+		}
+
+		var row []int64
+
+		for _, value := range record {
+			intVal, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("unable to parse float value: %v", err)
+			}
+			row = append(row, intVal)
+		}
+		result = append(result, row)
+	}
+
+	return result, nil
+}
+
+func readCSVToFloatSlice(filename string) ([]float64, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open file: %v", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	record, err := reader.Read()
+	if err != nil {
+		return nil, fmt.Errorf("unable to read CSV file: %v", err)
+	}
+
+	var result []float64
+
+	for _, value := range record {
+		floatVal, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse float value: %v", err)
+		}
+		result = append(result, floatVal)
+	}
+
+	return result, nil
+}
+
+func quantizeFeatures(borders []float64, unQuantizedFeatures []float64) []int64 {
+	numFeat := len(unQuantizedFeatures)
+	quantizedFeatures := make([]int64, 0, numFeat)
+	lenBorders := len(borders)
+
+	for i := 0; i < numFeat; i++ {
+		feature := unQuantizedFeatures[i]
+		count := 0
+
+		for count < lenBorders && borders[count] <= feature {
+			count++
+		}
+
+		quantizedFeatures = append(quantizedFeatures, int64(count))
+	}
+
+	return quantizedFeatures
+}
+
+func refTemplate(sampleQ []int64, mfbrTab [][]int64) []int64 {
+    var refTempPacked []int64
+    for _, i := range sampleQ {
+        refTemp := mfbrTab[i]
+        refTempPacked = append(refTempPacked, refTemp...)
+    }
+    return refTempPacked
+}
+
+func genVectOfInt(begin, length int) []int {
+	vect := make([]int, length)
+	for i := range vect {
+		vect[i] = begin + i
+	}
+	return vect
+}
+
+func vectIntPermutation(seed int64, begin, length int) []int {
+    r := rand.New(rand.NewSource(seed))
+    seed = r.Int63()
+
+	permuted := genVectOfInt(begin, length)
+
+	rand.Shuffle(len(permuted), func(i, j int) {
+		permuted[i], permuted[j] = permuted[j], permuted[i]
+	})
+
+	return permuted
+}
+
+func addSameValToVector(vect []int, val int) []int {
+	for i := range vect {
+		vect[i] += val
+	}
+	return vect
+}
+
+func genPermutationsConcat(seed int64, nPerm, lenPerm int) []int {
+	permutations := make([]int, 0, nPerm*lenPerm)
+
+	for i := 0; i < nPerm; i++ {
+		perm := vectIntPermutation(seed+int64(i), 0, lenPerm)
+		perm = addSameValToVector(perm, i*lenPerm)
+		permutations = append(permutations, perm...)
+	}
+
+	return permutations
+}
+
+func (Enrollment *Enrollment_s) encryptPermutedRefTempSingleCT(refTemp []int64, permutations []int) *rlwe.Ciphertext{
+    ringDim := Enrollment.params.N()
+    permRefTemp := make([]int64, ringDim)
+
+    for i := range refTemp {
+        permRefTemp[i] = refTemp[permutations[i]]
+    }
+    ptxt := bfv.NewPlaintext(Enrollment.params, Enrollment.params.MaxLevel())
+    Enrollment.encoder.Encode(permRefTemp, ptxt)
+    ctxt := bfv.NewCiphertext(Enrollment.params, 1, Enrollment.params.MaxLevel())
+    Enrollment.encryptor.Encrypt(ptxt, ctxt)
+
+    return ctxt
+}
+
+const SEED  = 54321
+const NFEAT = 128
+const NROWS = 8
+
 func main() {
-    ///////////////////////
-    size := 1 << 3
+    mfipPath := "../go/lookupTables/MFIP/MFIP_nB_3_dimF_128.csv"
+    lfwPath := "../go/data/LFW/Paul_McCartney/0.csv"
+    borderPath := "../go/lookupTables/Borders/Borders_nB_3_dimF_128.csv"
 
-	// Seed the random generator
-	rand.Seed(time.Now().UnixNano())
+    mfip, err := readCSVTo2DSlice(mfipPath)
+    if err != nil {
+        fmt.Println(fmt.Errorf(err.Error()))
+    }
+    lfw, err := readCSVToFloatSlice(lfwPath)
+    if err != nil {
+        fmt.Println(fmt.Errorf(err.Error()))
+    }
+    borders, err := readCSVToFloatSlice(borderPath)
+    if err != nil {
+        fmt.Println(fmt.Errorf(err.Error()))
+    }
 
-	// Generate the random list u
-	u := make([]int, size)
-	for i := 0; i < size; i++ {
-		u[i] = rand.Intn(size)
-	}
+    fmt.Println("MFIP", mfip)
 
-	// Create a 2D slice for the one-hot encoding
-	oneHot := make([][]int64, 3)
-	for i := range oneHot {
-		oneHot[i] = make([]int64, size)
-	}
+    //fmt.Println(lfw)
+    lfwQ := quantizeFeatures(borders, lfw)
+    fmt.Println("LFWQ", lfwQ)
 
-	// Set the appropriate positions to 1
-	for i := 0; i < 3; i++ {
-        for j := 0; j < size; j++ {
-            oneHot[i][u[j]] = 1
-        }
-	}
+    refTemp := refTemplate(lfwQ, mfip)
+    fmt.Printf("refTemp %v \n len(refTemp) %v \n", refTemp, len(refTemp))
 
-	// Flatten the one-hot encoded matrix
-	permProbeTempMask := flattenMatrix(oneHot)
+    permutations := genPermutationsConcat(SEED, NFEAT, NROWS)
+    fmt.Println("permutations", permutations)
 
-    ///////////////////////
-
-    const sliceSize = (1 << 3) * 3
-	const maxValue = 1 << 3
-
-	// Create a slice with the required size
-	permRefTempVec := make([]int64, sliceSize)
-
-	// Populate the slice with random permRefTempVec up to maxValue
-	for i := 0; i < sliceSize; i++ {
-		permRefTempVec[i] = int64(rand.Intn(maxValue))
-	}
-
-    ///////////////////////
-
-	paramsDef := bfv.PN13QP218
+    // Enryption
+    paramsDef := bfv.PN13QP218
     paramsDef.T = 0x3ee0001
 
-    // MinLogN in rlwe/params.go defined as 4 and Max 17
-    //paramsDef.LogN = 13
+    paramsDef.LogN = 10
 	params, err := bfv.NewParametersFromLiteral(paramsDef)
 	if err != nil {
 		panic(err)
@@ -129,33 +258,23 @@ func main() {
     rlk := kgen.GenRelinearizationKey(sk, 1)
 
     halfRing := params.N() / 2
-    maxRotation := bits.Len64(uint64(halfRing)) - 1
+    maxRotation := math.Log2(float64(halfRing))
 
     rotations := []int{}
-    for i := 0; i <= maxRotation; i++ {
+    for i := 0; i <= int(maxRotation); i++ {
         rotations = append(rotations, int(math.Pow(2, float64(i))))
     }
     rtks := kgen.GenRotationKeysForRotations(rotations, true, sk)
     evk := rlwe.EvaluationKey{Rlk: rlk, Rtks: rtks}
 
-
     Enrollment := &Enrollment_s{params, bfv.NewEncoder(params), bfv.NewEncryptor(params, pk)}
     BIP := &BIP_s{params, bfv.NewEvaluator(params, evk), bfv.NewDecryptor(params, sk)}
 
-    permRefTempPT := Enrollment.encoder.EncodeNew(permRefTempVec, params.MaxLevel())
-	permRefTempCT := Enrollment.encryptor.EncryptNew(permRefTempPT)
+    ctxt := Enrollment.encryptPermutedRefTempSingleCT(refTemp, permutations)
 
-    permDecPT := BIP.decryptor.DecryptNew(permRefTempCT)
-    permDec := Enrollment.encoder.DecodeUintNew(permDecPT)
-    fmt.Println("Decoded Ref       :", permDec[:100])
+    ptxt := BIP.decryptor.DecryptNew(ctxt)
+    refPerm := Enrollment.encoder.DecodeIntNew(ptxt)
 
-    finalScoreCT := BIP.getFinalScoreCT(Enrollment, permRefTempCT, permProbeTempMask)
-
-    finalScorePT := BIP.decryptor.DecryptNew(finalScoreCT)
-    finalScoreVec := Enrollment.encoder.DecodeUintNew(finalScorePT)
-
-    fmt.Println("Original Vector   :", permRefTempVec)
-	fmt.Println("Mask Vector       :", permProbeTempMask)
-    fmt.Printf("Final Score Vector: %v, len: %v\n", finalScoreVec[:16], len(finalScoreVec))
+    fmt.Println(refPerm)
 }
 
